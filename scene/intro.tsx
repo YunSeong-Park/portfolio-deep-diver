b677c14abc725df9de2871453c10208a58a58114
YunSeong Park <qkrdbstjd95@naver.com>
2022-05-15 17:28:14 +0900

    [New Feature] intro scene 구현

---
+++ scene/intro.tsx
@@ -0,0 +1,77 @@
+import { css } from "@emotion/react";
+import { Tween } from "react-gsap";
+import { Scene } from "react-scrollmagic";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import MoveSpan from "../components/moveSpan/MoveSpan";
+
+interface IntroProps {}
+
+const Intro: React.FC<IntroProps> = () => {
+  return (
+    <Scene triggerHook="onLeave" pin duration={300}>
+      {(progress: number) => {
+        return (
+          <div css={rootStyle}>
+            <Tween
+              to={{ opacity: 0, y: "-100px" }}
+              paused
+              totalProgress={progress}
+            >
+              <div css={phraseStyle}>
+                <p css={subTitleStyle}>
+                  "The Standard <br /> of completion in out opinion"
+                </p>
+                <div css={titleWrapperStyle}>
+                  <span css={titleStyle}>
+                    <MoveSpan text="Deep " fontSize="154px" />
+                    <span> &nbsp;Diver</span>
+                  </span>
+                </div>
+              </div>
+            </Tween>
+          </div>
+        );
+      }}
+    </Scene>
+  );
+};
+
+export default Intro;
+
+const rootStyle = css`
+  position: relative;
+`;
+
+const phraseStyle = css`
+  position: absolute;
+  left: 100px;
+  top: 180px;
+`;
+
+const titleWrapperStyle = css`
+  position: relative;
+  display: inline-block;
+`;
+
+const titleStyle = css`
+  display: flex;
+  font-size: 154px;
+  font-weight: 400;
+
+  :after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    bottom: -21px;
+    left: 0px;
+    width: 100%;
+    height: 2px;
+    background-color: #fff;
+  }
+`;
+
+const subTitleStyle = css`
+  font-weight: 400;
+  font-size: 68px;
+  line-height: 97px;
+`;
