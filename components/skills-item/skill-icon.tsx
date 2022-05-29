65c9b81eea8ea337e981fea4ef5db1a3313d613b
YunSeong Park <qkrdbstjd95@naver.com>
2022-05-29 19:07:56 +0900

    [New Feature] skill item 컴포넌트 구현

---
+++ components/skills-item/skill-icon.tsx
@@ -0,0 +1,38 @@
+import { css } from "@emotion/react";
+import Icon from "../icon/icon";
+
+type Skill = "photoshop" | "illustrator";
+
+interface SkillIconProps {
+  skill: Skill;
+}
+
+const SkillIcon: React.FC<SkillIconProps> = ({ skill }) => {
+  return (
+    <div css={rootStyle}>
+      <div>
+        <Icon icon={skill} />
+      </div>
+    </div>
+  );
+};
+
+export default SkillIcon;
+
+const rootStyle = css`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+
+  border: 1px solid #fff;
+  border-radius: 8px;
+  background-color: transparent;
+
+  div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
